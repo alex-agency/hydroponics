dbc99a7b0d88854d3d568b56f2ac5b1aa45ddfaa
Alex <alex.agency@gmail.com>
2015-05-27 10:49:56 +0300

    update

--- hydroponics.ino
+++ hydroponics.ino
@@ -620,9 +620,10 @@ void watering() {
   }
   // pause for cleanup pump and rest
   uint8_t pauseDuration = 5;
-  if(states[WARNING] == INFO_SUBSTRATE_DELIVERED ||
-      states[WARNING] == WARNING_SUBSTRATE_LOW)
-    pauseDuration = 20; // max officient pause
+  if(states[WARNING] == INFO_SUBSTRATE_DELIVERED)
+    pauseDuration = 19; // max officient pause
+  if(states[WARNING] == WARNING_SUBSTRATE_LOW)
+    pauseDuration = 22;
   // pause every 30 sec
   if((millis()/MILLIS_TO_SEC-startWatering) % 30 <= pauseDuration) {
     #ifdef DEBUG
